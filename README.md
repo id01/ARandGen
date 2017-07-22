65cc14a41bf21b530a2c4dfe0b2f5f3a99c324a9
id01 <gaviniboom@gmail.com>
2017-07-21 22:10:46 -0700

    modified:   README.md 	modified:   randgen.ino

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 ## ARandgen  
 A hardware random number generator using Arduino ADC noise.  
 No additional hardware required.  
-Generates data at about 170 bytes per second on an Arduino Uno.  
-Entropy ~ 7.45 (4096 bytes)  
+Generates data at about 320 B/s on an Arduino Uno.  
+Entropy ~ 7.97 (16 KB)  
--- randgen.ino
+++ randgen.ino
@@ -1,6 +1,6 @@
 int ppins = 6; // Number of physical analog pins
 bool randBit; // Current random bit
-unsigned char rTotal[2]; // Totals of random bits
+unsigned char rTotal; // Totals of random bits
 
 void setup() {
   Serial.begin(9600); // Use a slower baud rate as to not overload the processor
@@ -15,12 +15,9 @@ void loop() {
   // Do it twice to do XOR for more randomness
   for (int i=0; i<8; i++) {
     randBit = analogRead(i%ppins)%2;
-    rTotal[0] += randBit*pow(2,i);
-  }
-  for (int i=0; i<8; i++) {
-    randBit = analogRead(i%ppins)%2;
-    rTotal[1] += randBit*pow(2,i);
+    rTotal += randBit*pow(2,i);
   }
   // Write serial
-  Serial.write(rTotal[0] ^ rTotal[1]);
+  randomSeed(rTotal);
+  Serial.write(random(255));
 }
